367d4437182b26b9c788dacf5495cfab6d4ff318
DiesbecqAaron <la229719@student.helha.be>
2024-04-24 10:12:17 +0200

    Structure du projet + rapport semaine 1

---
+++ projetJavaFX/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module be.helha.poo3.projet.javafx.projetjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens be.helha.poo3.projet.javafx.projetjavafx to javafx.fxml;
+    exports be.helha.poo3.projet.javafx.projetjavafx;
+}
---
+++ projetJavaSpringBoot/src/test/java/be/helha/poo3/projet/springboot/projetjavaspringboot/ProjetJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package be.helha.poo3.projet.springboot.projetjavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

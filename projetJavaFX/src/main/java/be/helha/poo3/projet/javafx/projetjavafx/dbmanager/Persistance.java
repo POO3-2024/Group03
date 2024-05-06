f951571c7709046e06438d58a5a3234a7d0efdb3
DiesbecqAaron <la229719@student.helha.be>
2024-05-06 15:30:09 +0200

    ajout de la classe Persistance

---
+++ projetJavaFX/src/main/java/be/helha/poo3/projet/javafx/projetjavafx/dbmanager/Persistance.java
@@ -0,0 +1,17 @@
+package be.helha.poo3.projet.javafx.projetjavafx.dbmanager;
+
+public class Persistance {
+    private String type;
+    private String path;
+
+    public Persistance() {
+        super();
+    }
+
+    public String getType(){
+        return type;
+    }
+    public String getPath(){
+        return path;
+    }
+}

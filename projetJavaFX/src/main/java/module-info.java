a07ef6d407483e274795ece177af0de6bdfb0a45
DiesbecqAaron <la229719@student.helha.be>
2024-05-06 18:41:24 +0200

    Ajout de la classe DBManager et de ses méthodes getInstance et getConnexion + Ajout d'une ligne dans module-info.java pour java.sql

--- projetJavaFX/src/main/java/module-info.java
+++ projetJavaFX/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module be.helha.poo3.projet.javafx.projetjavafx {
 
     requires org.kordamp.bootstrapfx.core;
     requires com.google.gson;
+    requires java.sql;
 
     opens be.helha.poo3.projet.javafx.projetjavafx to javafx.fxml;
     exports be.helha.poo3.projet.javafx.projetjavafx;

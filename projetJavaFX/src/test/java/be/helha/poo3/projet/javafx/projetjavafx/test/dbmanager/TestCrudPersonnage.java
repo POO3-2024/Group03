5af7ed24f81f712806c4b0bd2488d6e2e3ad8c71
La227444 <La227444@student.helha.be>
2024-05-07 15:24:15 +0200

    creation de la methode 'testListPersonnages'

--- projetJavaFX/src/test/java/be/helha/poo3/projet/javafx/projetjavafx/test/dbmanager/TestCrudPersonnage.java
+++ projetJavaFX/src/test/java/be/helha/poo3/projet/javafx/projetjavafx/test/dbmanager/TestCrudPersonnage.java
@@ -1,6 +1,3 @@
-/**
- * @author Mohamed Samba Demba
- * */
 package be.helha.poo3.projet.javafx.projetjavafx.test.dbmanager;
 import be.helha.poo3.projet.javafx.projetjavafx.personnages.Gestionpersonnages;
 import be.helha.poo3.projet.javafx.projetjavafx.personnages.Personnage;
@@ -10,17 +7,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Order;
+
+/**
+ *
+ *@author Mohamed Samba Demba
+ *
+ */
 
 public class TestCrudPersonnage {
 
     Gestionpersonnages gestionpersonnages = new Gestionpersonnages();
-    private List<Personnage> personnages = new ArrayList<>();
+    private static final List<Personnage> personnages = new ArrayList<>();
+
     /**
      * Teste la méthode d'ajout de personnage.
      *
      * @throws Throwable Si une exception est levée pendant le test.
      */
     @Test
+    @Order(1)
     public void testAjoutPersonnage() throws Throwable {
         // Création de plusieurs personnages
         Personnage alice = new Personnage("Alice");
@@ -37,13 +43,44 @@ public class TestCrudPersonnage {
         personnages.add(charle);
 
         // Ajout des personnages à la base de donnée
-        boolean ajoutResulta = false;
+        boolean ajoutResultat = false;
         for (Personnage perso : personnages) {
-            ajoutResulta = gestionpersonnages.ajouterPerso(perso);
-            assertTrue(ajoutResulta);
+            ajoutResultat = gestionpersonnages.ajouterPerso(perso);
+            assertTrue(ajoutResultat);
         }
         //essaye d'ajoute un perso deja existant
-        ajoutResulta = gestionpersonnages.ajouterPerso(alice);
-        assertFalse(ajoutResulta);
+        ajoutResultat = gestionpersonnages.ajouterPerso(alice);
+        assertFalse(ajoutResultat);
+    }
+
+    /**
+     * Teste de la méthode listerPersonnages() de la classe Gestionpersonnages.
+     * Cette méthode compare les résultats obtenus avec la liste de personnages déjà
+     * présente et alimentée par le test précédent testAjouterPersonnage.
+     *
+     * @throws Throwable si une exception est levée pendant le test.
+     */
+
+    @Test
+    @Order(2)
+    public void testListPersonnages() throws Throwable {
+        // Récupération des personnages depuis la base de données
+        List<Personnage> personnagesResultat = gestionpersonnages.listerPersonnages();
+
+        // Vérification que les deux listes ont la même taille
+        assertEquals(personnages.size(), personnagesResultat.size());
+
+        // Parcours des deux listes en même temps pour comparer les personnages
+        for (int i = 0; i < personnages.size(); i++) {
+            // Récupération des personnages des deux listes
+            Personnage personnageAttendu = personnages.get(i);
+            Personnage personnageResultat = personnagesResultat.get(i);
+
+            // Comparaison des  personnages
+            assertEquals(personnageResultat.getName(),personnageAttendu.getName());
+            assertEquals(personnageResultat.getId(),personnageAttendu.getId());
+            assertEquals(personnageResultat.getManna(),personnageAttendu.getManna());
+            assertEquals(personnageResultat.getPointDeVie(),personnageAttendu.getPointDeVie());
+        }
     }
 }

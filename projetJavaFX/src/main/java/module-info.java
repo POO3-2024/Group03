fc85467141b8b0b41b975ffdb044e9b7ae20a652
Alahayne Abdel <la216549@student.helha.be>
2024-05-08 16:23:37 +0200

    Modification de la méthode ajouterArme + ajout du testAjoutArme

--- projetJavaFX/src/main/java/module-info.java
+++ projetJavaFX/src/main/java/module-info.java
@@ -6,6 +6,7 @@ module be.helha.poo3.projet.javafx.projetjavafx {
     requires com.google.gson;
     requires java.sql;
 
+
     opens be.helha.poo3.projet.javafx.projetjavafx to javafx.fxml;
     exports be.helha.poo3.projet.javafx.projetjavafx;
     exports be.helha.poo3.projet.javafx.projetjavafx.dbmanager;
---
+++ projetJavaFX/src/test/java/be/helha/poo3/projet/javafx/projetjavafx/test/dbmanager/testArmes.java
@@ -0,0 +1,48 @@
+package be.helha.poo3.projet.javafx.projetjavafx.test.dbmanager;
+
+import be.helha.poo3.projet.javafx.projetjavafx.armes.GestionArmes;
+import be.helha.poo3.projet.javafx.projetjavafx.armes.Armes;
+import javafx.fxml.FXML;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class testArmes {
+
+    GestionArmes gestionArmes = new GestionArmes();
+    private static final List<Armes> armes = new ArrayList<>();
+
+    @BeforeAll
+    static void initialisation() {
+        Armes epee = new Armes("Epée");
+        Armes arc = new Armes("Arc");
+        Armes lance = new Armes("Lance");
+        Armes hache = new Armes("Hache");
+        Armes pistolet = new Armes("Pistolet");
+
+        armes.add(epee);
+        armes.add(arc);
+        armes.add(lance);
+        armes.add(hache);
+        armes.add(pistolet);
+    }
+
+    @Test
+    @Order(1)
+    public void testAjoutArme(){
+        boolean ajoutResult = false;
+        for (Armes armes1 : armes){
+            ajoutResult = gestionArmes.ajouterArme(armes1);
+            assertTrue(ajoutResult);
+        }
+        ajoutResult = gestionArmes.ajouterArme(armes.get(0));
+        assertFalse(ajoutResult);
+    }
+
+}

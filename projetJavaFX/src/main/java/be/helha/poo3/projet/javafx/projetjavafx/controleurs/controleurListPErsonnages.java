0a16a935132c9a8faad357b0522bcaf893259230
La227444 <La227444@student.helha.be>
2024-05-08 17:27:53 +0200

    ajout de la methode listPers

--- projetJavaFX/src/main/java/be/helha/poo3/projet/javafx/projetjavafx/controleurs/controleurListPErsonnages.java
+++ projetJavaFX/src/main/java/be/helha/poo3/projet/javafx/projetjavafx/controleurs/controleurListPErsonnages.java
@@ -1,24 +1,43 @@
 package be.helha.poo3.projet.javafx.projetjavafx.controleurs;
 
+import be.helha.poo3.projet.javafx.projetjavafx.personnages.Gestionpersonnages;
+import be.helha.poo3.projet.javafx.projetjavafx.personnages.Personnage;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.function.Function;
+
 /**
  *
  *@author Mohamed Samba Demba
  *
  */
 public class controleurListPErsonnages {
+
     @FXML
     public Button btAddPerson;
+    @FXML
+    public VBox vbNom, vbPv, vbMana;
+    @FXML
+    public ListView lvNom, lvPv, lvMana;
+
+    private final Gestionpersonnages gestionpersonnages = new Gestionpersonnages();
+
+
+    private List<Personnage> personnages;
 
     public void initialize() {
+        this.listerPersonnages();
         btAddPerson.setOnAction(e -> {
             try {
                 openVueAddPersonnage();
@@ -26,6 +45,17 @@ public class controleurListPErsonnages {
                 throw new RuntimeException(ex);
             }
         });
+    }
+
+    private void listerPersonnages() {
+         personnages = gestionpersonnages.listerPersonnages();
+         for (Personnage personnage : personnages) {
+             lvNom.getItems().add(personnage.getName());
+             lvPv.getItems().add(personnage.getPointDeVie());
+             lvMana.getItems().add(personnage.getManna());
+
+         }
+
     }
 
     public void openVueAddPersonnage() throws IOException {

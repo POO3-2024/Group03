a5f98d93fcc10072fc077090177ee33ac2b007f9
Alahayne Abdel <la216549@student.helha.be>
2024-05-21 16:58:50 +0200

    Ajout des méthodes pour pouvoir supprimer une armes + ajout du popup pour confirmer

--- projetJavaFX/src/main/java/be/helha/poo3/projet/javafx/projetjavafx/controleurs/ControleurArme.java
+++ projetJavaFX/src/main/java/be/helha/poo3/projet/javafx/projetjavafx/controleurs/ControleurArme.java
@@ -1,5 +1,7 @@
 package be.helha.poo3.projet.javafx.projetjavafx.controleurs;
 
+import be.helha.poo3.projet.javafx.projetjavafx.domaine.Armes;
+import be.helha.poo3.projet.javafx.projetjavafx.domaine.Personnage;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -13,6 +15,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class ControleurArme implements Initializable {
@@ -47,10 +50,92 @@ public class ControleurArme implements Initializable {
     }
 
     private void initbtSupprimer() {
+        btSupprimer.setOnAction(e -> {
+            pnPopup.setVisible(true);
+        });
     }
 
     private void initbtConfirmation() {
+        btConfirmation.setOnAction(e -> {
+            try {
+                checkValidity("supprimer");
+                pnPopup.setVisible(false);
+                openVueArmes();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
     }
+
+    private void checkValidity(String operation) throws IOException{
+        String name = lbName.getText();
+        String degatsText = lbDegats.getText();
+        lbMessageError.setText("");
+        if (!allFieldsFilled(name, degatsText)) {
+            lbMessageError.setText("Veuillez remplir tous les champs.");
+            return;
+        }
+
+        int degats = parseInteger(degatsText);
+
+        if (!(degats >= 0)) {
+            lbMessageError.setText("Les valeurs doivent être des nombres positifs.");
+            return;
+        }
+        if(containsOnlyDigits(name)){
+            lbMessageError.setText("Le nom ne peut pas contenir que des chiffres.");
+            return;
+        }
+
+        Armes armesToAdd = new Armes(name);
+        armesToAdd.setId(idArmes);
+
+        if (armesToAdd.getDegats() < degats){
+            lbMessageError.setText("les dégats ne peuvent pas depasés "+ armesToAdd.getDegats()+".");
+            return;
+        }else {
+            armesToAdd.setDegats(degats);
+        }
+
+      
+    }
+
+    private boolean allFieldsFilled(String name, String degatsText) {
+        return !name.isEmpty() && !degatsText.isEmpty();
+    }
+
+    private int parseInteger(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            lbMessageError.setText("Veuillez entrer un nombre valide.");
+            return -1;
+        }
+    }
+
+    private boolean containsOnlyDigits(String text) {
+        for (char c : text.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     private void initBtRetour() {
         btRetour.setOnAction(e -> {

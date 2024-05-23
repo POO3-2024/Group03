2f0639aa653611c706e01ea317e81cfdc619ed50
La227444 <La227444@student.helha.be>
2024-05-23 13:43:00 +0200

    Ajout des methodes suivantes : 'initBtAttaquer','attaquerPersonnage', 'getPersonnageAAtaquer', 'getArme'

--- projetJavaFX/src/main/java/be/helha/poo3/projet/javafx/projetjavafx/controleurs/controleurAttaquer.java
+++ projetJavaFX/src/main/java/be/helha/poo3/projet/javafx/projetjavafx/controleurs/controleurAttaquer.java
@@ -4,6 +4,7 @@ import be.helha.poo3.projet.javafx.projetjavafx.daoImpl.ArmeDaoImpl;
 import be.helha.poo3.projet.javafx.projetjavafx.daoImpl.PersonnageDaoImpl;
 import be.helha.poo3.projet.javafx.projetjavafx.domaine.Armes;
 import be.helha.poo3.projet.javafx.projetjavafx.domaine.Personnage;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -11,6 +12,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -30,6 +32,11 @@ public class controleurAttaquer implements Initializable {
      */
     @FXML
     public Button btRetour, btAttaquer;
+    /**
+     *
+     */
+    @FXML
+    public Label lbMessageErreur;
 
     /**
      * Gestionnaire des personnages et des armes de l'application.
@@ -53,7 +60,57 @@ public class controleurAttaquer implements Initializable {
         initBtRetour();
         initListePersannages();
         initListeArmes();
+        initBtAttaquer();
     }
+
+    /**
+     * Initialise le gestionnaire d'événements pour le bouton de Attaquer.
+     */
+    private void initBtAttaquer() {
+        btAttaquer.setOnAction(e -> {
+                attaquerPersonnage();
+        });
+    }
+    /**
+    *
+     */
+    private void attaquerPersonnage(){
+       Personnage persoAttaquer = getPersonnageAAtaquer();
+       Armes arme = getArme();
+       if(persoAttaquer != null && arme!=null){
+           int oldPv = persoAttaquer.getPointDeVie();
+           if (oldPv == 0){
+               lbMessageErreur.setText("les pv du personnage sont à  0");
+               return;
+           }else {
+               int newPointdeVie = oldPv - arme.getDegats();
+               persoAttaquer.setPointDeVie(newPointdeVie);
+               if (personnageDaoImpl.modifierPersonnage(persoAttaquer)){
+                   lbMessageErreur.setText("Les points de vie de "+ persoAttaquer.getName() +" sont passé de " + oldPv+ " à "+ persoAttaquer.getPointDeVie()+ "\n"+ arme.getNom()+" a fait "+ arme.getDegats() +" de dégats");
+               }
+           }
+       }else{
+           lbMessageErreur.setText(" Veuillez choisir une arme et un personnage");
+       }
+
+    }
+    private Personnage getPersonnageAAtaquer(){
+        for (Personnage personnage : personnages) {
+            if (personnage.getName().equals(cbPersonnage.getValue())){
+                return personnage;
+            }
+        }
+        return null;
+    }
+    private Armes getArme(){
+        for (Armes arme : armes) {
+            if (arme.getNom().equals(cbArme.getValue())){
+                return arme;
+            }
+        }
+        return null;
+    }
+
     /**
      * Initialise la liste des personnages.
      */
@@ -99,7 +156,6 @@ public class controleurAttaquer implements Initializable {
 
     /**
      * Charge et affiche une nouvelle vue.
-     *
      * @param path le chemin de la vue à charger.
      * @throws IOException si une erreur d'entrée/sortie se produit lors de la lecture de la vue.
      */
